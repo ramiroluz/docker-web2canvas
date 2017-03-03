9481aa422cc841f9dca5f8867c371240b3121a26
Claudio Ferreira Filho <filhocf@gmail.com>
2017-03-03 15:13:20 -0300

    Adjust parameters_<port>.py into initial script

--- run.sh
+++ run.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
-# Get Web2py Admin Password
-if [ "$nopassword" -eq 0 ]
-then
-  echo -e "Web2py Admin Password: \c "
-  read -s PW
-  printf "\n"  # fix no new line artifact of "read -s" to avoid cleartext password
-fi
 
 # Set Web2py Admin Password
 cd /usr/local/web2py
-if [ "$nopassword" -eq 0 ]
-then
-su - www-data python -c "from gluon.main import save_password; save_password('$PW',443)"
+if [ ! $PASSWORD ]
+  then export PASSWORD="changeme"
 fi
-
+python -c "from gluon.main import save_password; save_password('$PASSWORD',443)"
+ln -s parameters_443.py parameters_80.py
+chown www-data.www-data parameters_*.py
 
 nginx
 uwsgi --master --die-on-term --emperor /etc/uwsgi --logto /var/log/uwsgi/uwsgi.log &
